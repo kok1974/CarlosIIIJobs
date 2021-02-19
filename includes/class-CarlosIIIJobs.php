847d7a8552bdfc49177cfac0b72a49c5f2a00ec2
kok1974 <8818473@alu.murciaeduca.es>
2021-02-19 08:53:31 +0100

    algo mas

--- includes/class-CarlosIIIJobs.php
+++ includes/class-CarlosIIIJobs.php
@@ -174,6 +174,8 @@ class CarlosIIIJobs {
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
 
+		$this->loader->add_action( 'admin_post_nopriv_CarlosIIIJob_suscribe', $plugin_admin, 'CarlosIIIJob_suscribe' );
+        $this->loader->add_action( 'admin_post_CarlosIIIJob_suscribe', $plugin_admin, 'CarlosIIIJob_suscribe' );
 	}
 
 	/**
